629255b40194e3e80b95ebe7259efc982bbd0180
Eppo <eppesuig08@gmail.com>
2025-03-29 17:28:48 +0100

    Added Bluetooth + updated app

--- bot.cpp
+++ bot.cpp
@@ -1,24 +1,16 @@
 #include <MeMCore.h>
 #include <Arduino.h>
-#include <Wire.h>
 #include <SoftwareSerial.h>
-#include "src/bluetoothhc.h"
-#include "src/HC06.h"
 
-BluetoothClassHC bluetoothhc;
-
+SoftwareSerial bluetoothSerial(0, 1);
 MeRGBLed rgbled_7(7, 2);
 
 MeDCMotor motor_9(9);
 MeDCMotor motor_10(10);
 
-String carattere = "";
-int speed = 150;
-
 void move(int direction, int speed) {
   int leftSpeed = 0;
   int rightSpeed = 0;
-
   if(direction == 1) {
     leftSpeed = speed;
     rightSpeed = speed;
@@ -37,52 +29,56 @@ void move(int direction, int speed) {
 }
 
 void setup() {
-  Serial.begin(9600); 
+  Serial.begin(115200);
+  bluetoothSerial.begin(115200);
   
-  bluetoothhc.inithc(0,1,"BTSlave",1234);
-
   rgbled_7.fillPixelsBak(0, 2, 1);
-  rgbled_7.setColor(0, 51, 0, 255);
+  rgbled_7.setColor(0, 0, 0, 0);
   rgbled_7.show();
 }
 
 void loop() {
-  if (bluetoothhc.dataLineAvailableHc()) {
-    carattere = bluetoothhc.readDataLineHc();
-
-    rgbled_7.setColor(0, 255, 0, 0);
-    rgbled_7.show();
-
-    delay(200);
-
-    rgbled_7.setColor(0, 0, 0, 0);
-    rgbled_7.show();
-
-    Serial.print("Carattere: ");
-    Serial.println(carattere);
+  if (bluetoothSerial.available()) {
+    char receivedChar = bluetoothSerial.read();
     
-    if(carattere == "f") {
-      move(1, speed);
-      delay(3);
+
+    if (receivedChar == '\r' || receivedChar == '\n') {
+      return;
+    }
+
+    Serial.print("Carattere ricevuto: ");
+    Serial.println(receivedChar);
+
+    if (receivedChar == 'f') {
+      rgbled_7.setColor(0, 0, 255, 0);
+      rgbled_7.show();
+
+      move(1, 90 / 100.0 * 255);
+      delay(1000);
       move(1, 0);
-    } else if(carattere == "b") {
-      move(2, speed);
-      delay(3);
+    } else if (receivedChar == 'b') {
+      rgbled_7.setColor(0, 255, 0, 0);
+      rgbled_7.show();
+
+      move(2, 90 / 100.0 * 255);
+      delay(1000);
       move(2, 0);
-    } else if(carattere == "l") {
-      move(3, speed);
-      delay(3);
+    } else if (receivedChar == 'l') {
+      rgbled_7.setColor(0, 255, 0, 0);
+      rgbled_7.show();
+
+      move(3, 80 / 100.0 * 255);
+      delay(500);
       move(3, 0);
-    } else if(carattere == "r") {
-      move(4, speed);
-      delay(3);
+    } else if (receivedChar == 'r') {
+      rgbled_7.setColor(0, 255, 0, 0);
+      rgbled_7.show();
+
+      move(4, 80 / 100.0 * 255);
+      delay(500);
       move(4, 0);
     }
+
+    delay(200);
   }
-
-  _loop();
-}
-
-void _loop() {
-  bluetoothhc.bluetoothLoopHc();
 }
