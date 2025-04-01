32f2d7f54bd870c5d85d86d66d3b9b1c1e07d77c
Eppo <eppesuig08@gmail.com>
2025-04-01 19:59:07 +0200

    App finita + aggiunto buzzer

--- bot.cpp
+++ bot.cpp
@@ -5,6 +5,10 @@
 SoftwareSerial bluetoothSerial(0, 1);
 MeRGBLed rgbled_7(7, 2);
 
+MeUltrasonicSensor ultrasonic_1(1);
+
+MeBuzzer buzzer;
+
 MeDCMotor motor_9(9);
 MeDCMotor motor_10(10);
 
@@ -38,47 +42,44 @@ void setup() {
 }
 
 void loop() {
+  int distanza = ultrasonic_1.distanceCm(); 
+
+  if(distanza < 20){
+    buzzer.tone(988, 0.25 * 1000);
+  }
+
   if (bluetoothSerial.available()) {
     char receivedChar = bluetoothSerial.read();
     
-
     if (receivedChar == '\r' || receivedChar == '\n') {
       return;
     }
-
-    Serial.print("Carattere ricevuto: ");
-    Serial.println(receivedChar);
-
+    
     if (receivedChar == 'f') {
       rgbled_7.setColor(0, 0, 255, 0);
       rgbled_7.show();
-
       move(1, 90 / 100.0 * 255);
       delay(1000);
       move(1, 0);
     } else if (receivedChar == 'b') {
       rgbled_7.setColor(0, 255, 0, 0);
       rgbled_7.show();
-
       move(2, 90 / 100.0 * 255);
       delay(1000);
       move(2, 0);
     } else if (receivedChar == 'l') {
       rgbled_7.setColor(0, 255, 0, 0);
       rgbled_7.show();
-
       move(3, 80 / 100.0 * 255);
       delay(500);
       move(3, 0);
     } else if (receivedChar == 'r') {
       rgbled_7.setColor(0, 255, 0, 0);
       rgbled_7.show();
-
       move(4, 80 / 100.0 * 255);
       delay(500);
       move(4, 0);
     }
-
     delay(200);
   }
 }
